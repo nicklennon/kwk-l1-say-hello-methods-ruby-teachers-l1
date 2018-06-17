4005484426e7113ba7a9c8c67225440bb7091895
nicklennon <nicklennon.53@gmail.com>
2018-06-17 16:09:55 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,17 @@
 # Build your say_hello method here
+
+def say_hello(name = "Ruby Programmer" )
+  puts "Hello #{name}!"
+end
+
+
+# Test Cases
+
+say_hello()
+say_hello
+say_hello("Nick")
+say_hello("Nick Lennon")
+
+# puts "What is your name?"
+# name = gets.strip
+# say_hello(name)
